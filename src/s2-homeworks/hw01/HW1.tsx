d0db9f9ad163d2eeec8d80cfbdee2c5ca5f8fc78
ElenaKuklina <cuklina.lena@gmail.com>
2024-01-07 16:26:15 +0300

    Doing homework hw01.

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -6,25 +6,35 @@ import FriendMessage from './friend-message/FriendMessage'
 import avatar from './avatar.png'
 
 /*
-* 1 - описать тип MessageType
-* 2 - описать тип MessagePropsType в файле Message.tsx
-* 3 - в файле Message.tsx отобразить приходящие данные
-* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
-* 5 - сделать стили в соответствии с дизайном
+* 1 - описать тип MessageType +++
+* 2 - описать тип MessagePropsType в файле Message.tsx +++
+* 3 - в файле Message.tsx отобразить приходящие данные +++
+* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx +++
+* 5 - сделать стили в соответствии с дизайном +++
 * */
 
-// нужно создать правильный тип вместо any
-export type MessageType = any
+// нужно создать правильный тип вместо any +++
+export type MessageType = {
+    id: number
+    user: {
+        avatar: string
+        name: string
+    },
+    message: {
+        text: string
+        time: string
+    },
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        name: 'Leo',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
+        text: 'Hello, she didn’t do anything and rested all day, how are you?', // можно менять
         time: '22:00', // можно менять
     },
 }
@@ -32,11 +42,11 @@ export const friendMessage0: MessageType = {
     id: 100,
     user: {
         avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        name: 'Lucas', // можно менять
     },
     message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
-        time: '22:00', // можно менять
+        text: 'Hello, how are you, what did you do yesterday?', // можно менять
+        time: '22:31', // можно менять
     },
 }
 
