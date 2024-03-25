4a73453c227098a46866526291678fc6400b2174
ElenaKuklina <cuklina.lena@gmail.com>
2024-03-25 16:29:22 +0300

    Doing homework hw05. refactoring code

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -32,11 +32,8 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        //className={open ? `${s.nav} ${sidebarClass}` : `${s.close}`}
-                        className={open ? `${s.nav}` + `${s.background}` : `${s.close}`}
-                        //className={s.active ? s.background : s.close}
-                        // className={s.active ? ` ${s.close} ${s.background}` : `${s.close}`}
-                        // className={...} // делает студент
+
+                        className={open ? `${s.nav}` + `${s.background}` : `${s.close}`} // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -44,9 +41,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        //className={open ? `${s.nav} ${sidebarClass}` : `${s.close}`}
-                        className={open ? `${s.nav}` + `${s.background}` : `${s.close}`}
-                        // className={...} // делает студент
+                        className={open ? `${s.nav}` + `${s.background}` : `${s.close}`} // делает студент
                     >
                         Junior
                     </NavLink>
@@ -54,9 +49,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={open ? `${s.nav} ${sidebarClass}` : `${s.close}`}
-                        className={open ? `${s.nav}` + `${s.background}` : `${s.close}`}
-                        // className={...} // делает студент
+                        className={open ? `${s.nav}` + `${s.background}` : `${s.close}`} // делает студент
                     >
                         Junior Plus
                     </NavLink>
