aa1a2efad5436d61c01d3b2310cf3d9770bc7bb7
ElenaKuklina <cuklina.lena@gmail.com>
2025-03-18 01:58:00 +0300

    fix: выровнена длина полосок слайдеров

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,23 +5,20 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                // Стили для активной полосы (трека)
-                color: 'rgba(1, 203, 34, 1)',
+                color: '#00CC22',
 
-                // Стили для неактивной полосы (трека)
-                '& .MuiSlider-track': {
-                    backgroundColor: 'gba(1, 203, 34, 1)',
-                },
-
-                // Стили для ползунка (thumb)
                 '& .MuiSlider-thumb': {
-                    backgroundColor: 'rgba(1, 203, 34, 1)',
-                    border: '2px solid rgba(1, 203, 34, 1)',
+                    border: '1px solid #00CC22',
+                    color: '#FFFFFF',
+                    '&:before': {
+                        height: '6px',
+                        width: '6px',
+                        backgroundColor: '#00CC22',
+                        boxShadow: 'none'
+                    }
                 },
-
-                // Стили для rail (неактивная полоса)
                 '& .MuiSlider-rail': {
-                    backgroundColor: 'rgba(139, 139, 139, 1)',
+                    color: '#8B8B8B',
                 },
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
