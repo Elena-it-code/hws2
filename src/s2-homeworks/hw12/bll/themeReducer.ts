1abf9a83f40dd1e974c9beb8906728fc1f593c9c
ElenaKuklina <cuklina.lena@gmail.com>
2025-03-26 16:15:09 +0300

    feat(HW12): реализация переключения тем
    - Доработан редьюсер для управления темой с типами
    - Подключен выбор темы через useSelector
    - Реализована функция change для смены темы
    - Настроен SuperSelect с передачей пропсов
    - Обновлены стили для поддержки тем

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,22 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+// Создаём тип на основе initState
+export type ThemeState = typeof initState
+
+export const themeReducer = (state: ThemeState = initState, action: ChangeThemeIdActionType): ThemeState => {
     switch (action.type) {
         // дописать
+        case "SET_THEME_ID" : {
+            return {...state, themeId: action.id}
+        }
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number) => ({type: 'SET_THEME_ID', id} as const)
+
+// Action type
+export type ChangeThemeIdActionType = ReturnType<typeof changeThemeId>
